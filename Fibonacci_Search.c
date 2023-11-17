9894ae9edeef8c24353988774766f244f39e0678
sameer240704 <sameergupta79711@gmail.com>
2023-11-17 19:34:28 +0530

    Added Fibonacci Search Algorithm

--- Fibonacci_Search.c
+++ Fibonacci_Search.c
@@ -1,61 +1,56 @@
-#include <stdio.h>
-int min(int, int);
-int fibonacci_search(int[], int, int);
-int min(int a, int b){
-    return (a > b) ? b : a;
-}
+#include<stdio.h>
+#include<conio.h>
+int minimum(int, int );
+int fibonacci_search(int [], int, int);
 
-int fibonacci_search(int arr[], int n, int key){
-    int offset = -1;
-    int Fm2 = 0;
-    int Fm1 = 1;
-    int Fm = Fm2 + Fm1;
-    while (Fm < n) {
-        Fm2 = Fm1;
-        Fm1 = Fm;
-        Fm = Fm2 + Fm1;
-    }
-    while (Fm > 1) {
-        int i = min(offset + Fm2, n - 1);
-        if (arr[i] < key) {
-            Fm = Fm1;
-            Fm1 = Fm2;
-            Fm2 = Fm - Fm1;
-            offset = i;
-        } else if (arr[i] > key) {
-            Fm = Fm2;
-            Fm1 = Fm1 - Fm2;
-            Fm2 = Fm - Fm1;
-        } else {
-            return i;
-        }
-        printf("\n");
-        for(int j=0 ; j<n ; j++) {
-            if(j==i)
-                printf("|%d| ", arr[j]);
-            else    
-                printf("%d ", arr[j]);
-        }
-        printf("\n");
-    }
-    if (Fm1 && arr[offset + 1] == key)
-        return offset + 1;
-    return -1;
-}
-int main(){
-    int i, key, pos, length;
-    printf("Enter the length of your array : ");
+int main() {
+    int i, length, key;
+    printf("\nEnter the length of the array : ");
     scanf("%d", &length);
     int arr[length];
-    printf("\nEnter the SORTED ARRAY : \n");
+    printf("\nEnter the SORTED ARRAY : ");
     for(i=0 ; i<length ; i++) {
         scanf("%d", &arr[i]);
     }
     printf("\nEnter the element to be searched : ");
     scanf("%d", &key);
-   pos = fibonacci_search(arr, length, key);
+    int pos = fibonacci_search(arr, length, key);
     if(pos >= 0)
-        printf("The element is found at index %d", pos);
-    else
-        printf("Unsuccessful Search");
+        printf("\nElement %d found at position : %d", key, (pos+1));
+    else 
+        printf("\nElement not found");
+    return 0;
+}
+
+int minimum(int a, int b) {
+    return (a > b) ? b : a;
+}
+
+int fibonacci_search(int arr[], int length, int key) {
+    int offset = -1, Fm, Fm1 = 1, Fm2 = 0, i;
+    Fm = Fm1 + Fm2;
+    while(Fm < length) {
+        Fm2 = Fm1;
+        Fm1 = Fm;
+        Fm = Fm1 + Fm2;
+    }
+    while(Fm > 1) {
+        i = minimum(offset + Fm2, length - 1);
+        if(arr[i] < key) {
+            Fm = Fm1;
+            Fm1 = Fm2;
+            Fm2 = Fm - Fm1;
+            offset = i;
+        }
+        else if(arr[i] > key) {
+            Fm = Fm2;
+            Fm1 -= Fm2;
+            Fm2 = Fm - Fm1;
+        }
+        else 
+            return i;
+    }
+    if(Fm1 && arr[offset+1] == key)
+        return (offset + 1);
+    return -1;
 }
